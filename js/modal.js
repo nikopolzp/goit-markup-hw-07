6374e64704fd653b8d59ede0717ca7e5aecd7ed9
Ruslan P <nikopolzp@gmail.com>
2022-11-05 00:52:38 +0200

    add scss fox index.html

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,20 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openMenuBtn: document.querySelector('[data-modal-open]'),
+    closeMenuBtn: document.querySelector('[data-modal-close]'),
+    menu: document.querySelector('[data-modal]'),
+    body: document.querySelector('body'),
+    // menuList: document.querySelector('.mob-menu-list'),
   };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle('backdrop--is-hidden');
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  // refs.menuList.addEventListener('click', removeMenu);
+  function toggleMenu() {
+    refs.menu.classList.toggle('backdrop--is-hidden');
+    refs.body.classList.toggle('no-scroll');
   }
+  // function removeMenu() {
+  //   refs.menu.classList.add('is-hidden');
+  //   refs.body.classList.remove('no-scroll');
+  // }
 })();
