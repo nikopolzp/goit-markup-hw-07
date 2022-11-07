8907905008e7074fc3eca76c7f04edf3e4e066cf
Ruslan P <nikopolzp@gmail.com>
2022-11-07 20:37:05 +0200

    refactiring

--- js/modal.js
+++ js/modal.js
@@ -10,7 +10,7 @@
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
   // refs.menuList.addEventListener('click', removeMenu);
   function toggleMenu() {
-    refs.menu.classList.toggle('backdrop--is-hidden');
+    refs.menu.classList.toggle('is-hidden');
     refs.body.classList.toggle('no-scroll');
   }
   // function removeMenu() {
